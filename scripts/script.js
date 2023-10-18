688bcaae8223c7f288555dfd08e548812ccb3b1b
LeandroFerrero <leaferrero@gmail.com>
2023-10-18 16:21:31 -0300

    agregado alert si la cantidad no es valida

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,13 @@ var descuento =[0.2, 0.5,0.85];
 function calcular(){
   let cant = parseInt(document.getElementById("cantidad").value);
   let cat = parseInt(document.getElementById("categoria").value);
-  precioDescuento = precio * descuento[cat];
-  return cant * precioDescuento;
+  if(Number.isInteger(cant) && cant !==0){
+    precioDescuento = precio * descuento[cat];
+    return cant * precioDescuento;
+  } else {
+    alert("Debe ingresar una cantidad valida y distinta de 0")
+    return 0
+  }
 }
 
 
